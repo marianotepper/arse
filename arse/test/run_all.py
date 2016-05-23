23265d345a95b665be6a1f2cfe408f60d2e34d16
marianotepper <Lulita75>
2016-05-23 12:45:15 +0200

    Added test_stereo to the run_all list

--- arse/test/run_all.py
+++ arse/test/run_all.py
@@ -3,10 +3,12 @@ import test_vp
 import test_pozzoveggiani
 import test_piazza_bra
 import test_adelaide
+import test_stereo
 
 test_2d.run_all()
 test_vp.run_all()
 test_pozzoveggiani.run_all()
 test_piazza_bra.run_all()
 test_adelaide.run_all()
+test_stereo.run_all()
 
