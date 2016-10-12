Fixed error in the computation of the distances to centroid

arse/pme/utils.py
@@ -13,13 +13,13 @@ def normalize_2d(points):
         warnings.warn('Found points at infinity', RuntimeWarning)
 
     new_points = np.copy(points)
-    # For the finite points ensure homogeneous coords have scale of 1
+    # Ensure that homogeneous coord is 1
     new_points[finite_idx, 0] /= points[finite_idx, 2]
     new_points[finite_idx, 1] /= points[finite_idx, 2]
     new_points[finite_idx, 2] = 1
 
     centroid = np.mean(new_points[finite_idx, :2], axis=0)
-    dist = np.linalg.norm(new_points[finite_idx, :2] - centroid, ord=0, axis=1)
+    dist = np.linalg.norm(new_points[finite_idx, :2] - centroid, ord=2, axis=1)
     scale = np.sqrt(2) / np.mean(dist)
 
     trans = np.array([[scale, 0, 0],